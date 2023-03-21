513c302328212a5e9dec585440be8464494f3933
clowndogloko <124565600+clowndogloko@users.noreply.github.com>
2023-03-21 04:36:00 -0600

    Initialize build

---
+++ libs/core/src/index.ts
@@ -0,0 +1,6 @@
+
+export * from "./lib/cli";
+export * from "./lib/core";
+export { Package } from "./lib/package";
+export { Project } from "./lib/project";
+export { default as tempWrite } from "./lib/temp-write";
